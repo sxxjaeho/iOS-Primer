5b344728c69a01cc22790219c0f7731cd4acab7c
sxxjaeho <jaeho_ace@foxmail.com>
2024-03-17 17:23:30 +0800

    Updated arithmetic primer

--- contents/arithmetic/code/路径总和.playground/Contents.swift
+++ contents/arithmetic/code/路径总和.playground/Contents.swift
@@ -10,6 +10,13 @@ import UIKit
  输入：root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
  输出：true
  
+      5
+     / \
+    4   8
+   /    /\
+  11   13 4
+  /\       \
+ 7  2       1
  时间复杂度：O(n) 空间复杂度：O(n)
  */
 
@@ -42,15 +49,26 @@ func hasPathSum(_ root: TreeNode?, _ targetSum: Int) -> Bool {
                 return true
             }
             
-            if let rightNode = node.right {
-                nodeQueue.append(rightNode)
-                sumQueue.append(sum + rightNode.val)
-            }
             if let leftNode = node.left {
                 nodeQueue.append(leftNode)
                 sumQueue.append(sum + leftNode.val)
+                print(leftNode.val, sumQueue)
             }
+            if let rightNode = node.right {
+                nodeQueue.append(rightNode)
+                sumQueue.append(sum + rightNode.val)
+                print(rightNode.val, sumQueue)
+            }
+            // nodeQueue = [4, 8], sumQueue = [9, 13]
+            // nodeQueue = [8, 11], sumQueue = [13, 20]
+            // nodeQueue = [11, 13, 4], sumQueue = [20, 26, 17]
+            // nodeQueue = [13, 4, 7, 2], sumQueue = [26, 17, 27, 22]
+            // nodeQueue = [7, 2, 1], sumQueue = [27, 22, 18]
+            // nodeQueue = [2, 1], sumQueue = [22, 18]
         }
     }
     return false
 }
+
+let root = TreeNode(val: 5, left: TreeNode(val: 4, left: TreeNode(val: 11, left: TreeNode(val: 7, left: nil, right: nil), right: TreeNode(val: 2, left: nil, right: nil)), right: nil), right: TreeNode(val: 8, left: TreeNode(val: 13, left: nil, right: nil), right: TreeNode(val: 4, left: nil, right: TreeNode(val: 1, left: nil, right: nil))))
+print(hasPathSum(root, 22))
